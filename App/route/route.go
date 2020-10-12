8f39fb300a5a9990ff329eb39aaa62a54533162a
Muhammad Fahrul <fahrul.m@gmail.com>
2020-10-12 11:12:23 +0700

    Remove cors middleware
    cors handling removed to apigateway

--- App/route/route.go
+++ App/route/route.go
@@ -2,7 +2,6 @@ package route
 
 import (
 	"context"
-	"fmt"
 	"net/http"
 
 	"github.com/go-kit/kit/log"
@@ -35,7 +34,6 @@ func Run(ctx context.Context, db *mongo.Database, logger log.Logger) error {
 	e := config.NewConfig()
 	r := gin.New()
 	r.Use(gin.Recovery())
-	r.Use(corsMiddleware())
 
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -57,22 +55,4 @@ func Run(ctx context.Context, db *mongo.Database, logger log.Logger) error {
 	})
 
 	return r.Run() // listen and serve on 0.0.0.0:8080
-}
-
-//corsMiddleware CORS
-func corsMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Access-Control-Allow-Origin, Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		if c.Request.Method == "OPTIONS" {
-			fmt.Println("OPTIONS")
-			c.AbortWithStatus(200)
-		} else {
-			c.Next()
-		}
-	}
 }
